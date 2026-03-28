3a301e279749c9df23e8806e21fa4cc53b7f7d13
Faith Chinaza <159724381+Pharmfaithrin@users.noreply.github.com>
2026-03-28 23:36:13 +0100

    Update pharmacy_database.sql
    Here,i created a relational database/table which we will query in the pharmacy_sales_analysis.sql file repo,in oder to understand how sales has been in the pharmacy within the specified years.

--- pharmacy_database.sql
+++ pharmacy_database.sql
@@ -1,1 +1,34 @@
+Databasae tool used: MySQL Workbench
+  Analysis is done in the 'pharmacy_sale_analysis.sql' file
+  
+*Understanding the Project Goal
 
+I am creating/analyzing a pharmacy’s data to answer:
+
+1. Which drugs sell the most?
+2. How much revenue is generated monthly?
+3. Who are the top customers?
+4. Are there expired drugs still in stock?
+
+-- Create table
+CREATE TABLE pharmacy_sales (
+    sale_id INT,
+    drug_name VARCHAR(100),
+    category VARCHAR(50),
+    quantity INT,
+    price DECIMAL(10,2),
+    sale_date DATE,
+    customer_id INT,
+    expiry_date DATE
+);
+
+-- Insert sample data
+INSERT INTO pharmacy_sales VALUES
+(1, 'Paracetamol', 'Analgesic', 10, 500, '2026-01-10', 101, '2027-01-01'),
+(2, 'Amoxicillin', 'Antibiotic', 5, 1500, '2026-01-11', 102, '2025-12-01'),
+(3, 'Vitamin C', 'Supplement', 20, 300, '2026-02-01', 103, '2026-08-01'),
+(4, 'Paracetamol', 'Analgesic', 15, 500, '2026-02-05', 101, '2027-01-01'),
+(5, 'Ibuprofen', 'Analgesic', 8, 700, '2026-03-01', 104, '2025-10-01');
+
+-- query table content
+SELECT * FROM pharmacy_sales;
